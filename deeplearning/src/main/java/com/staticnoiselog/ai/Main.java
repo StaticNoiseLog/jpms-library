017983a86c79eeca038dce76415324924460ebeb
StaticNoiseLog <beat.kappert@gmail.com>
2024-03-15 21:36:17 +0100

    Gradle 8.6 and newer IntelliJ version

--- deeplearning/src/main/java/com/staticnoiselog/ai/Main.java
+++ deeplearning/src/main/java/com/staticnoiselog/ai/Main.java
@@ -2,6 +2,6 @@ package com.staticnoiselog.ai;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("I am a workaround for 'package is empty or does not exist' build errors.");
     }
 }
